ceef97232286f12fc031c6af736595a2275c5b2e
Jansen Kwong <j.kwong4@student.unimelb.edu.au>
2019-04-20 23:56:01 +1000

    working prototype, just no actual pic of hangman

--- hangman.py
+++ hangman.py
@@ -17,39 +17,62 @@ def formBoard(word):
             display.append('_')
     return display
 
-def print_board(category, arr):
+def print_board(category, arr, wrong):
     print("Your Category: "+ category)
     print(" ".join(arr))
+    print("Guessed wrong letters:", wrong)
+
+def updateBoard(letter, word, board):
+        for i in range(0, len(word)):
+            if(letter == word[i]):
+                board[i] = letter
+
+def hangMan(mistake):
+    ## using a formatter; width = 10
+    
+    return 0
     
 
 def main():
+    ## mistakes
+    mistake = []
+
     ## Generate list
     listing = developList()
 
     ##Pick random key
-    pickedInd = random.randint(0,len(listing.keys()))
+    pickedInd = random.randint(0,2)
 
     ##Category
     category = list(listing.keys())[pickedInd]
 
     ## pick word
     wordInd = random.randint(0,len(list(listing[category])))
-    word = listing[category][wordInd].lower()
+    word = listing[category][wordInd].upper()
 
     ##convert to empty strings
     display = formBoard(word)
 
-    ## print a display
-    print_board(category,display)
+    while("".join(display) != word):
+        ## print a display
+        print_board(category,display, mistake)
 
-    playerInput = input("Pick a letter: ")
+        playerInput = input("Pick a letter: ")
+        playerInput = playerInput.upper()
 
-    if playerInput in word:
-        for i in range(0, len(word)):
-            if(playerInput == word[i]):
-                display[i] = playerInput
+        ## update the board
+        if playerInput in word:
+            updateBoard(playerInput, word, display)
+        else:
+            mistake.append(playerInput)
 
-    print_board(category,display)
+        if len(mistake) == 5:
+            print("Game Over.")
+            print("The word was:",word)
+            break
+        print_board(category,display, mistake)
+
+
 
 if __name__ == "__main__":
     main()
